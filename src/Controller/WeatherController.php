c77642dc72c439ead3ed675ef8ec59ac2ddfe28d
dhmm <dhmm@outlook.com>
2024-08-05 13:57:16 +0300

    Section 11: Practice: Dummy Data for Forecast

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -12,9 +12,46 @@ class WeatherController extends AbstractController
     #[Route('/{countryCode}/{cityName}')]
     public function forecast(string $countryCode, string $cityName): Response
     {
+        $forecasts = [
+            [
+                "date" => new \DateTime('2024-01-01'),
+                "temperatureCelsius" => 17,
+                "flTemperatureCelsius" => 16,
+                "pressure" => 1000,
+                "humidity" => 64,
+                "wind_speed" => 3.2,
+                "wind_deg" => 270,
+                "cloudiness" => 75,
+                'icon' => 'sun',
+            ],
+            [
+                "date" => new \DateTime('2024-01-02'),
+                "temperatureCelsius" => 12,
+                "flTemperatureCelsius" => 13,
+                "pressure" => 1001,
+                "humidity" => 62,
+                "wind_speed" => 2.2,
+                "wind_deg" => 180,
+                "cloudiness" => 50,
+                'icon' => 'cloud',
+            ],
+            [
+                "date" => new \DateTime('2024-01-03'),
+                "temperatureCelsius" => 11,
+                "flTemperatureCelsius" => 12,
+                "pressure" => 1010,
+                "humidity" => 63,
+                "wind_speed" => 3.5,
+                "wind_deg" => 270,
+                "cloudiness" => 15,
+                'icon' => 'cloud-rain',
+            ],
+        ];
+
         return $this->render('weather/forecast.html.twig',[
+            'forecasts' => $forecasts,
             'countryCode' => $countryCode,
-            'cityName' => $cityName
+            'city' => $cityName
         ]);
     }
 }
